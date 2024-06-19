1d25197f05e25743e3944afaa9a823269eed1cdc
Vedikaashinde <134995578+Vedikaashinde@users.noreply.github.com>
2024-06-19 16:05:29 +0530

    Add files via upload

---
+++ CoundOfDigit.java
@@ -0,0 +1,16 @@
+package NumberProgram;
+
+public class CoundOfDigit {
+	
+	public static void main(String[] args) {
+		
+		int num=123654;
+		int ct=0;
+		while (num!=0) {
+			num/=10;
+			ct++;
+		}
+		System.out.println("The count of number is : "+ct);
+	}
+
+}
---
+++ EvenOdd.java
@@ -0,0 +1,27 @@
+package NumberProgram;
+
+import java.util.Scanner;
+
+public class EvenOdd extends EvenOddWithoutMod {
+	
+	public static void main(String[] args) {
+		
+		Scanner scanner = new Scanner(System.in);
+		System.out.println("Enter Any Number : ");
+		int num=scanner.nextInt();
+		
+		if (num%2==0) {
+			System.out.println("Number is even");
+		} else {
+			System.out.println("Number is odd");
+		}
+		scanner.close();
+		
+		
+	}
+	public static void display()
+	{
+		System.out.println("heyyy");
+	}
+
+}
